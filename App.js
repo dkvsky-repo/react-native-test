fb0ba5207e48e787d4487ea1ad6a6a7a831059cc
David Kontorovsky <1487550+dkvsky-repo@users.noreply.github.com>
2022-09-25 10:27:54 -0700

    Creating card component

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
+import Card from './app/components/Card/Card';
 import colors from './app/config/colors';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-      <WelcomeScreen />
+    <View style={styles.container}>
+      <Card 
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+      // <WelcomeScreen />
     // <ViewImageScreen />
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   justifyContent: 'center'
-  // }
+  container: {
+    backgroundColor: '#f8f4f4',
+    padding: 20,
+    paddingTop: 100
+  }
 });
